ed8c2ff9049c7d930b8bc7eff1243d8b5b81933a
Preethi <preethi@cognitivemobile.net>
2018-11-01 12:13:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/preethi/intentservicesample/CashbackIntentService.java
@@ -0,0 +1,40 @@
+package com.example.preethi.intentservicesample;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Preethi on 10/11/2018.
+ */
+
+public class CashbackIntentService extends IntentService {
+    final static String CASHBACK_INFO = "cashback_info";
+    public CashbackIntentService() {
+        super("Cashback IntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String cb_category = intent.getStringExtra("cashback_cat");
+
+        String cbinfo = getCashbackInfo(cb_category);
+        sendCashbackInfoToClient(cbinfo);
+    }
+    private String getCashbackInfo(String cbcat){
+        String cashback;
+        if("electronics".equals(cbcat)){
+            cashback = "Upto 20% cashback on electronics";
+        }else if("fashion".equals(cbcat)){
+            cashback = "Upto 60% cashbak on all fashion items";
+        }else{
+            cashback = "All other categories except fashion and electronics, flat 30% cashback";
+        }
+        return cashback;
+    }
+    private void sendCashbackInfoToClient(String msg){
+        Intent intent = new Intent();
+        intent.setAction(CASHBACK_INFO);
+        intent.putExtra("cashback",msg);
+        sendBroadcast(intent);
+    }
+}
---
+++ app/src/main/java/com/example/preethi/intentservicesample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.preethi.intentservicesample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CashbackReciver cashbackReciver;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.cb_results);
+        registerCashbackReceiver();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(cashbackReciver);
+    }
+    public void startCashbackService(View view){
+        EditText et = (EditText) findViewById(R.id.cashback_cat);
+
+        Intent cbIntent =  new Intent();
+        cbIntent.setClass(this, CashbackIntentService.class);
+        cbIntent.putExtra("cashback_cat", et.getText().toString());
+        startService(cbIntent);
+    }
+    private void registerCashbackReceiver(){
+        cashbackReciver = new CashbackReciver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(CashbackIntentService.CASHBACK_INFO);
+
+        registerReceiver(cashbackReciver, intentFilter);
+    }
+    private class CashbackReciver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String cbinfo = intent.getStringExtra("cashback");
+            tv.setText(cbinfo);
+        }
+    }
+}
